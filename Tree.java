768f96bbaf9717b7fbf2767ee2214cb9599d1d1b
Zane Dunn <60718015+dunndeft@users.noreply.github.com>
2022-04-20 20:18:54 +0000

    Initial commit

---
+++ Tree.java
@@ -0,0 +1,17 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // Introduce self and print tree
+    System.out.println("Hi, everyone! I’m Zane and I’m learning to code in Java.");
+    System.out.println("Let's plant a tree!");
+    System.out.println(" ");
+    System.out.println("     *     ");
+    System.out.println("    ***    ");
+    System.out.println("   *****   ");
+    System.out.println("  *******  ");
+    System.out.println("    | |    ");
+    System.out.println("   _|_|_   ");
+    System.out.println("  #######  "); 
+  }
+}
+// Print "java class name" e.g. java Tree in the Console, then hit Enter.
